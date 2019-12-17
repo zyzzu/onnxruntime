d92f5f6dd1ce9b8a37c6a9b1e2d3b82c33a83cb9
Ori Levari <orlevari@microsoft.com>
2019-12-17 14:39:46 -0800

    fix unsigned/signed comparisons

--- include/onnxruntime/core/session/onnxruntime_cxx_inline.h
+++ include/onnxruntime/core/session/onnxruntime_cxx_inline.h
@@ -361,7 +361,7 @@ template <>
 inline Value Value::CreateTensor<std::string>(const OrtMemoryInfo*, std::string* p_data, size_t p_data_element_count, const int64_t* shape, size_t shape_len) {
   // convert the array of std::string to an array of const char *
   std::vector<const char*> string_vector;
-  for (int i = 0; i < p_data_element_count; ++i) {
+  for (size_t i = 0; i < p_data_element_count; ++i) {
     string_vector.push_back(p_data[i].c_str());
   }
   // now make an empty tensor using the default allocator (strings have to make a copy)
@@ -456,7 +456,7 @@ inline std::vector<std::string> Value::GetStrings() {
   std::vector<size_t> offsets(shape[0]);
   this->GetStringTensorContent(buf.data(), buf.size(), offsets.data(), offsets.size());
   // now go build all the strings
-  for (auto i = 0; i < shape[0]; ++i) {
+  for (size_t i = 0; i < shape[0]; ++i) {
     std::string str;
     size_t strlen = 0;
     // are we on the last one?
