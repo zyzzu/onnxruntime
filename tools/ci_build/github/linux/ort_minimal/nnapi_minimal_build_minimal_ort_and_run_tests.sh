f5100ee04a6734020ce0e66a391c14ec91f1559c
gwang <wanggy@outlook.com>
2020-11-23 12:15:33 -0800

    fix

--- tools/ci_build/github/linux/ort_minimal/nnapi_minimal_build_minimal_ort_and_run_tests.sh
+++ tools/ci_build/github/linux/ort_minimal/nnapi_minimal_build_minimal_ort_and_run_tests.sh
@@ -31,7 +31,6 @@ python3 $ORT_ROOT/tools/ci_build/build.py \
     --build_dir $MIN_BUILD_DIR \
     --config Debug \
     --skip_submodule_sync \
-    --parallel \
     --cmake_generator=Ninja \
     --use_nnapi \
     --android \
@@ -39,7 +38,6 @@ python3 $ORT_ROOT/tools/ci_build/build.py \
     --android_ndk_path $ANDROID_HOME/ndk-bundle \
     --android_abi=x86_64 \
     --android_api=29 \
-    --build_java \
     --minimal_build extended \
     --disable_rtti \
     --disable_ml_ops \
